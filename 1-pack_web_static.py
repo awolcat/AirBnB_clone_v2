7e9fcef1f4d31a5c06e5c86d1c7e3ec86310d3f3
Albert <albertirura11@gmail.com>
2023-09-07 16:42:49 +0300

    compress web_static with fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,10 +10,13 @@ def do_pack():
         in the target host containing the contents
         of web_static compressed
     """
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_path = f'versions/web_static_{time}.tgz'
+
+    """prepare directory for archive"""
     local("mkdir -p versions")
-    result = local("tar cf versions/web_static_$(date '+%Y%m%d%H%M%S').tgz\
-                   /data/web_static/*")
-    if (result.succeeded):
-        archive = local("ls -t versions/web_static_* | head -1")
-        return archive.stdout
+    """create the archive"""
+    result = local(f"tar cf {archive_path} /data/web_static/*")
+    if result.succeeded:
+        return archive_path
     return None
