7a3b035c5f39be41d6521dfaa7ccdc12a13ac2a3
Albert <albertirura11@gmail.com>
2023-09-07 15:43:31 +0300

    compress web_static files with fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,9 +4,6 @@
 """
 from fabric.api import *
 
-env.user = 'ubuntu'
-env.hosts = ['100.26.234.115', '52.91.132.168']
-
 
 def do_pack():
     """This method will create a folder versions
@@ -14,8 +11,8 @@ def do_pack():
         of web_static compressed
     """
     local("mkdir -p versions")
-    result = local("tar cf versions/web_static_$(date '+%Y%m%d%H%M%S').tar
-                   /data/web_static/*")
+    result = local("""tar cf versions/web_static_$(date '+%Y%m%d%H%M%S').tar
+                   /data/web_static/*""")
     if (result.succeeded):
         archive = local("ls -t versions/ | head -1")
         return archive.stdout
