d6db1480d23e7741aa903b82e895ecb873a1bb0d
Albert <albertirura11@gmail.com>
2023-09-07 21:51:04 +0300

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,27 +8,27 @@ from datetime import datetime
 from fabric.api import put, run, env, local
 
 
-env.user = 'ubuntu'
-env.hosts = ['100.26.234.115', '52.91.132.168']
-env.key_filename = '~/.ssh/school'
-
-
 def do_pack():
     """This method will create a folder versions
         in the target host containing the contents
         of web_static compressed
     """
     time = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_path = f'versions/web_static_{time}.tgz'
+    archive_path = 'versions/web_static_{}.tgz'.format(time)
     try:
         """prepare directory for archive"""
         local("mkdir -p versions")
         """create the archive"""
-        result = local(f"tar cvzf {archive_path} web_static/")
+        result = local("tar cvzf {} web_static/".format(archive_path))
         return archive_path
         # return f'web_static_{time}.tgz'
     except Exception:
         return None
+
+
+env.user = 'ubuntu'
+env.hosts = ['100.26.234.115', '52.91.132.168']
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
