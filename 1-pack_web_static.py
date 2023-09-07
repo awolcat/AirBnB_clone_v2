f499b525e6fd344620b8f786b46f7f26b956e9ba
Albert <albertirura11@gmail.com>
2023-09-07 16:32:15 +0300

    compress web_static with fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 """This module defines a fabfile which  generates a .tgz archive
    from the contents of the web_static folder of the AirBnB Clone
 """
-from fabric.api import *
+from fabric.api import local
 
 
 def do_pack():
@@ -11,7 +11,7 @@ def do_pack():
         of web_static compressed
     """
     local("mkdir -p versions")
-    result = local("tar cf versions/web_static_$(date '+%Y%m%d%H%M%S').tar\
+    result = local("tar cf versions/web_static_$(date '+%Y%m%d%H%M%S').tgz\
                    /data/web_static/*")
     if (result.succeeded):
         archive = local("ls -t versions/ | head -1")
