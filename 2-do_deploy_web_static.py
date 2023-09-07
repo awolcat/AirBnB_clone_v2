b77288941e0f1533f08653b1804e7da3c65bfd06
Albert <albertirura11@gmail.com>
2023-09-07 20:47:54 +0300

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
 """This module defines a fabfile which  generates a .tgz archive
    from the contents of the web_static folder of the AirBnB Clone
+   and uses it to deploy the website to our webservers
 """
-from fabric.api import put, run, env
 import os
+from datetime import datetime
+from fabric.api import put, run, env, local
 
 
 env.user = 'ubuntu'
 env.hosts = ['100.26.234.115', '52.91.132.168']
 env.key_filename = '~/.ssh/school'
+
+
+def do_pack():
+    """This method will create a folder versions
+        in the target host containing the contents
+        of web_static compressed
+    """
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_path = f'versions/web_static_{time}.tgz'
+    try:
+        """prepare directory for archive"""
+        local("mkdir -p versions")
+        """create the archive"""
+        result = local(f"tar cvzf {archive_path} web_static/")
+        return archive_path
+        # return f'web_static_{time}.tgz'
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
