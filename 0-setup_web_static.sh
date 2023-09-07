84566ca1fae82247313da6cb81694bf141220230
Albert <albertirura11@gmail.com>
2023-09-07 10:55:01 +0300

    setup nginx web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,8 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Create document directories
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
 
 # Create html document
 echo "<html>
@@ -16,17 +16,17 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current/
 
 # Change owner of document directory hierarchy
-chown -R "$USER":"$USER" /data/
+sudo chown -R "$USER":"$USER" /data/
 
 # Edit the config file
 
-sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart nginx
 sudo service nginx restart
