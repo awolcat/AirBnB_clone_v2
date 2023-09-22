448a59b88fc80676265e6f87732710ed39568c8e
Albert <albertirura11@gmail.com>
2023-09-22 13:56:37 +0300

    the simplest flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    This module defines the simplest flask app possible
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """ This function defines the response
+        to a http request for the root of our website
+    """
+    return "Hello HBNB!"
+
+app.run(host='0.0.0.0', port=5000)
