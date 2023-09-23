9b4f023bc800df68c742c044fd34db4def40ed8d
Albert <albertirura11@gmail.com>
2023-09-23 17:22:36 +0300

    jinja conditionals

---
+++ web_flask/9-states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module defines a flask app
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def play_with_states(id=None):
+    """This method will display all states
+        or a specific state given a valid state id
+        together with its associated cities
+    """
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, id=id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
